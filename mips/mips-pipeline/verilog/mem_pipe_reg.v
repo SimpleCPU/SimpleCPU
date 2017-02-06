f1a88b0527c9c733cfd75c097b22c70473bf1c79
Rahul Behl <raulbehl@users.noreply.github.com>
2017-02-06 11:19:32 +0530

    [RTL] Fixed signal width
    The signal width for res_alu_mem_pipe_reg signal was incorrect
    Fixed it to be a 32-bit signal

--- mips/mips-pipeline/verilog/mem_pipe_reg.v
+++ mips/mips-pipeline/verilog/mem_pipe_reg.v
@@ -7,17 +7,17 @@ module mem_pipe_reg
         input   wire        reg_wr_mem_pipe_reg_i,
         input   wire        mem_to_reg_mem_pipe_reg_i,
         input   wire        mem_wr_mem_pipe_reg_i,
-        input   wire        res_alu_mem_pipe_reg_i,
+        input   wire[31:0]  res_alu_mem_pipe_reg_i,
         output  wire        reg_wr_mem_pipe_reg_o,
         output  wire        mem_to_reg_mem_pipe_reg_o,
         output  wire        mem_wr_mem_pipe_reg_o,
-        output  wire        res_alu_mem_pipe_reg_o
+        output  wire[31:0]  res_alu_mem_pipe_reg_o
     );
 
     reg        reg_wr_mem_pipe_reg;
     reg        mem_to_reg_mem_pipe_reg;
     reg        mem_wr_mem_pipe_reg;
-    reg        res_alu_mem_pipe_reg;
+    reg[31:0]  res_alu_mem_pipe_reg;
 
     assign reg_wr_mem_pipe_reg_o         =  reg_wr_mem_pipe_reg;
     assign mem_to_reg_mem_pipe_reg_o     =  mem_to_reg_mem_pipe_reg;
