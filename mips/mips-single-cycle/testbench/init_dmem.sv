80411f2eb18f1f4adf64837eaae7a3b2b031a68e
Rahul Behl <raulbehl@gmail.com>
2017-02-07 00:37:21 +0530

    [TB] Updated TB for branch instructions     - Updated the init_imem module to make use of the pc_values hex file for initialising imem     - Also, updated the new data and text range in the RTL

--- mips/mips-single-cycle/testbench/init_dmem.sv
+++ mips/mips-single-cycle/testbench/init_dmem.sv
@@ -1,7 +1,7 @@
 // Memory initialisation
 
 function void init_dmem ();
-    for (int i = 0; i < 32'h4000; i=i+1)
+    for (int i = 0; i < 32'h100000; i=i+1)
     begin
         T1.D_MEM1.dmem [i] = 32'hefefefef;
     end
--- mips/mips-single-cycle/testbench/init_imem.sv
+++ mips/mips-single-cycle/testbench/init_imem.sv
@@ -2,13 +2,15 @@
 
 function void init_imem ();
     logic [31:0] instr_hex [1023:0];
-    logic [31:0] pc_val [1023:0];
+    logic [31:0] pc_val [2048:0];
     
     $readmemh ("instr_hex", instr_hex, 0, 999);
+    $readmemh ("pc_values_hex", pc_val, 0, 999);
     
-    for (int i = 0; i < 999; i++) 
+    for (int i = 0; instr_hex[i]; i++) 
     begin
-        T1.I_MEM1.imem [i] = instr_hex[i];
+        //$display ("Loading %x at %x\n", instr_hex[i], pc_val[i]);
+        T1.I_MEM1.imem [pc_val[i]>>2] = instr_hex[i];
     end
 
 endfunction
