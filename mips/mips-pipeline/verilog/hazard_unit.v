3b481cb01791477fe7f826e958bafcfddb1391e7
raulbehl <raulbehl@gmail.com>
2017-02-21 00:25:20 +0530

    [RTL] - Minor updates to hazard unit

--- mips/mips-pipeline/verilog/hazard_unit.v
+++ mips/mips-pipeline/verilog/hazard_unit.v
@@ -1,1 +1,19 @@
-// TODO: Add a hazard unit to manage register forwarding
+// Hazard checking unit 
+
+module hazard_unit
+    (
+        input   wire[4:0]   rs_iss_ex_hz_i,
+        input   wire[4:0]   rt_iss_ex_hz_i,
+        input   wire[4:0]   rs_ex_mem_hz_i,
+        input   wire[4:0]   rt_ex_mem_hz_i,
+        input   wire        mem_to_reg_ex_mem_hz_i,
+        input   wire        reg_wr_mem_wb_hz_i,
+        input   wire        reg_wr_wb_ret_hz_i,
+        output  wire        stall_fetch_hz_o,
+        output  wire        stall_iss_hz_o,
+        output  wire        flush_ex_hz_o,
+        output  wire[1:0]   fwd_p1_ex_mem_hz_o,
+        output  wire[1:0]   fwd_p2_ex_mem_hz_o
+    );
+
+endmodule
