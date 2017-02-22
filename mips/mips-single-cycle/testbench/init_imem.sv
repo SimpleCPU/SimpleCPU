5784b38c25d1f1b0e48ed80ee6355ea017e1ffc9
raulbehl <raulbehl@gmail.com>
2017-02-22 09:08:53 +0530

    [TB] Added valueplusarg to get the test name from cmd line

--- mips/mips-single-cycle/testbench/init_imem.sv
+++ mips/mips-single-cycle/testbench/init_imem.sv
@@ -3,9 +3,18 @@
 function void init_imem ();
     logic [31:0] instr_hex [1023:0];
     logic [31:0] pc_val [2048:0];
-    
-    $readmemh ("instr_hex", instr_hex, 0, 999);
-    $readmemh ("pc_values_hex", pc_val, 0, 999);
+
+    string instr_hex_s;
+    string pc_values_hex_s;
+    string test_name;
+
+    if (!($value$plusargs("test=%s", test_name)))
+        $fatal ("No test name given");
+    instr_hex_s     = {test_name, ".hex"};
+    pc_values_hex_s = {test_name, "_pc.hex"};
+
+    $readmemh (instr_hex_s, instr_hex, 0, 999);
+    $readmemh (pc_values_hex_s, pc_val, 0, 999);
     
     for (int i = 0; instr_hex[i]; i++) 
     begin
