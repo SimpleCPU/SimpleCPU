2c6839dfa5408c0f63334d539a1d4ae9080d9f23
bubble12 <guptasanchi2007@gmail.com>
2017-02-22 16:09:55 +0530

    Updated run.pl to pick the tests from tests/ folder

--- mips/mips-single-cycle/run.pl
+++ mips/mips-single-cycle/run.pl
@@ -11,6 +11,17 @@ GetOptions (
     'test=s'        =>  \$test_name
     );
 
+my $dir_name = $test_name;
+if ($dir_name =~ /basic/) {
+  $dir_name =~ s/_basic//;
+}
+elsif ($dir_name =~ /stress/) {
+  $dir_name =~ s/_stress//;
+}
+# get the test from hex-gen
+print ("cp ../hex_gen/tests/rand_$dir_name/hex/$test_name* ");
+system ("cp ../hex_gen/tests/rand_$dir_name/hex/$test_name* .");
+
 # compile the ISS first
 chdir '../iss';
 print ("make iss\n");
@@ -35,5 +46,5 @@ else {
     # Run the simulation
     print ("vsim -c top_tb -sv_lib iss -do \"run -all; exit\" +test=$test_name  | tee sim.log");
     system ("vsim -c top_tb -sv_lib iss -do \"run -all; exit\" +test=$test_name | tee sim.log");
-    system ("rm -rf vsim.wlf wlf* transcript ");
+    system ("rm -rf vsim.wlf wlf* transcript $test_name.hex $test_name\_pc.hex");
 }
