e1071740afa292e91ab634af0006c3a6e25ba305
dhruv behl <dhruvbhl@gmail.com>
2017-02-24 16:52:05 +0530

    Added support for logging failed test executions

--- mips/scripts/regressionManager.py
+++ mips/scripts/regressionManager.py
@@ -1,8 +1,11 @@
 import os
+import re
+import time
 import shutil
 import subprocess
 from pexpect import fdpexpect
 
+Logfilename = str(int(time.time()))
 
 def regression():
 	main_dir = '../mips-single-cycle/'
@@ -11,7 +14,7 @@ def regression():
 	sub_dir = os.listdir(test_dir)
 	hex_dir = ''
 	pc = 'pc'
-
+	testfile = open(Logfilename, "w+")
 	for s in sub_dir:
 		stress_test = []
 		basic_test = []
@@ -32,23 +35,41 @@ def regression():
 			for files in basic_test:
 				if pc not in files:
 					print "\n" + "*" * 10 + "Running tests from: " + hex_dir + "*" * 10 + "\n"
-					child = fdpexpect.fdspawn(subprocess.call("perl " + main_dir + "run.pl -regress 1 -test " + hex_dir + files.split('.')[0], shell=True))
-					c = child.expect(['Fatal: TEST FAILED'])
-					if c == 1:
-						child.kill(0)
+					cmd = "perl " + main_dir + "run.pl -regress 1 -test " + hex_dir + files.split('.')[0]
+					subprocess.Popen(cmd, shell = True, stdout=testfile, stderr=testfile)
 		
 		if stress_test:
 			for files in stress_test:
 				if pc not in files:
 					print "\n" + "*" * 10 + "Running tests from: " + hex_dir + "*" * 10 + "\n"
-					child = fdpexpect.fdspawn(subprocess.call("perl " + main_dir + "run.pl -regress 1 -test " + hex_dir + files.split('.')[0], shell=True))
-					c = child.expect(['Fatal: TEST FAILED'])
-					if c == 1:
-						child.kill(0)
+					cmd = "perl " + main_dir + "run.pl -regress 1 -test " + hex_dir + files.split('.')[0]
+					subprocess.Popen(cmd, shell = True, stdout=testfile,stderr=testfile)
 
+	testfile.close()
+
+def parseLog():
+	Logfile = open(Logfilename,'r+')
+	test = r"# Loading ..\/hex_gen\/tests\/(.*)"
+	testfile = ''
+	regex = r"# TEST PASSED"
+	match = 0
+	for line in Logfile:
+		matchtest = re.match(test, line)
+		if matchtest:
+			testfile = matchtest.group(1)
+			print testfile
+		if re.match(regex, line):
+			match = match + 1
+			print "[ERROR] " + testfile +" failed during regression run, Logging results in " + Logfilename
+			
+	if match == 0:
+		print "[DEBUG] No tests failed, no log files created"
+		os.remove(Logfilename)
+	Logfile.close()
 
 def main():
 	regression()
+	parseLog()
 
 if __name__ == '__main__':
 	main()
