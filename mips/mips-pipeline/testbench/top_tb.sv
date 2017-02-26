65c885056f1ea59ca09bf645adf8bbd336fec2a0
raulbehl <raulbehl@gmail.com>
2017-02-26 15:36:45 +0530

    [TB] Added pipelined signals
        - Tapped RTL signals at various pipe stages
        - Qualified all the signals till RET stage

--- mips/mips-pipeline/testbench/top_tb.sv
+++ mips/mips-pipeline/testbench/top_tb.sv
@@ -9,28 +9,82 @@ import "DPI-C" function void run (int cycles);
 import "DPI-C" function int compare_r (int pc, int instr, int rd, int rs, int rt, int rd_val, int rs_val, int rt_val);
 import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs_val, int rt_val);
 
-    wire[31:0]  pc;
-    wire[31:0]  instr;
-    wire[4:0]   rd;
-    wire[4:0]   rs;
-    wire[4:0]   rt;
-    wire[31:0]  rd_val;
-    wire[31:0]  rs_val;
-    wire[31:0]  rt_val;
-    wire[31:0]  rd_val_dest;
-    wire[31:0]  rt_val_dest;
-    reg clk_tb, reset_tb;
+    logic clk_tb, reset_tb;
 
-    assign pc           = T1.curr_pc_top;
-    assign instr        = T1.instr_top;
-    assign rd           = T1.rd_top;
-    assign rs           = T1.rs_top;
-    assign rt           = T1.rt_top;
-    assign rd_val       = T1.R1.reg_file[rd];
-    assign rs_val       = T1.R1.reg_file[rs];
-    assign rt_val       = T1.R1.reg_file[rt];
-    assign rt_val_dest  = T1.reg_wr_top ? T1.wr_data_rf_top : rt_val;
-    assign rd_val_dest  = T1.reg_wr_top ? T1.wr_data_rf_top : rd_val;
+    // Fetch stage signals
+    logic[31:0]   pc_fetch;
+    logic[31:0]   instr_fetch;
+    // Issue stage signals
+    logic[31:0]   pc_iss;
+    logic[31:0]   instr_iss;
+    logic         is_r_type_iss;
+    logic         is_i_type_iss;
+    logic         is_j_type_iss;
+    logic[4:0]    rd_iss;
+    logic[4:0]    rs_iss;
+    logic[4:0]    rt_iss;
+    // Execute stage signals
+    logic[31:0]   pc_ex;
+    logic[31:0]   instr_ex;
+    logic         is_r_type_ex;
+    logic         is_i_type_ex;
+    logic         is_j_type_ex;
+    logic[4:0]    rd_ex;
+    logic[4:0]    rs_ex;
+    logic[4:0]    rt_ex;
+    logic[31:0]   rd_val_ex;
+    logic[31:0]   rs_val_ex;
+    logic[31:0]   rt_val_ex;
+    // Memory stage signals
+    logic[31:0]   pc_mem;
+    logic[31:0]   instr_mem;
+    logic         is_r_type_mem;
+    logic         is_i_type_mem;
+    logic         is_j_type_mem;
+    logic[4:0]    rd_mem;
+    logic[4:0]    rs_mem;
+    logic[4:0]    rt_mem;
+    logic[31:0]   rd_val_mem;
+    logic[31:0]   rs_val_mem;
+    logic[31:0]   rt_val_mem;
+    // Write-back stage signals
+    logic[31:0]   pc_wb;
+    logic[31:0]   instr_wb;
+    logic         is_r_type_wb;
+    logic         is_i_type_wb;
+    logic         is_j_type_wb;
+    logic[4:0]    rd_wb;
+    logic[4:0]    rs_wb;
+    logic[4:0]    rt_wb;
+    logic[31:0]   rd_val_wb;
+    logic[31:0]   rs_val_wb;
+    logic[31:0]   rt_val_wb;
+    logic[31:0]   rd_val_dest_wb;
+    logic[31:0]   rt_val_dest_wb;
+    
+
+    // FETCH
+    assign pc_fetch         = T1.pc_pc_reg_fetch;
+    assign instr_fetch      = T1.instr_pc_reg_fetch;
+
+    // ISSUE
+    assign is_r_type_iss    = T1.is_r_type_iss_ex;
+    assign is_i_type_iss    = T1.is_i_type_iss_ex;
+    assign is_j_type_iss    = T1.is_j_type_iss_ex;
+    assign rs_iss           = T1.rs_iss_ex;
+    assign rt_iss           = T1.rt_iss_ex;
+    assign rd_iss           = T1.rd_iss_ex;
+
+    // EXECUTE
+    assign rd_val_ex        = T1.reg_file[rd_ex];
+    assign rs_val_ex        = T1.reg_file[rs_ex];
+    assign rt_val_ex        = T1.reg_file[rt_ex];
+
+    // MEM
+
+    // WRITE-BACK
+    //assign rt_val_dest  = T1.reg_wr_top ? T1.wr_data_rf_top : rt_val;
+    //assign rd_val_dest  = T1.reg_wr_top ? T1.wr_data_rf_top : rd_val;
 
     top T1 (
         .clk (clk_tb),
@@ -59,29 +113,75 @@ import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs
         # (T/2);
     end
 
-    always @ (negedge clk_tb)
-    if (~reset_tb)
+    // ISSUE
+    always @ (posedge clk_tb)
     begin
-        run (1);
-        if (T1.is_r_type_top) 
-        begin
-            if (!compare_r (pc, instr, rd, rs, rt, rd_val_dest, rs_val, rt_val))
-                $finish;
-        end
-        else if (T1.is_i_type_top)
-        begin
-            if (!compare_i (pc, instr, rs, rt, rs_val, rt_val_dest))
-                $finish;
-        end
+        pc_iss      <=  pc_fetch;
+        instr_iss   <=  instr_fetch;
     end
 
-    always @ (negedge clk_tb)
+    // EXECUTE
+    always @ (posedge clk_tb)
     begin
-        if ((T1.instr_top == 'hc) && (T1.R1.reg_file[2] == 'ha))
-        begin
-            $display("End of simulation reached\n");
-            $finish;
-        end
+        pc_ex           <=  pc_iss;
+        instr_ex        <=  instr_iss;
+        is_r_type_ex    <=  is_r_type_iss;
+        is_i_type_ex    <=  is_i_type_iss;
+        is_j_type_ex    <=  is_j_type_iss;
+        rs_ex           <=  rs_iss;
+        rt_ex           <=  rt_iss;
+        rd_ex           <=  rd_iss;
     end
+
+    // MEMORY
+    always @ (posedge clk_tb)
+    begin
+        pc_mem          <=  pc_ex;
+        instr_mem       <=  instr_ex;
+        is_r_type_mem   <=  is_r_type_ex;
+        is_i_type_mem   <=  is_i_type_ex;
+        is_j_type_mem   <=  is_j_type_ex;
+        rs_mem          <=  rs_ex;
+        rt_mem          <=  rt_ex;
+        rd_mem          <=  rd_ex;
+    end
+
+    // WRITE-BACK
+    always @ (posedge clk_tb)
+    begin
+        pc_wb           <=  pc_mem;
+        instr_wb        <=  instr_mem;
+        is_r_type_ex    <=  is_r_type_mem;
+        is_i_type_ex    <=  is_i_type_mem;
+        is_j_type_ex    <=  is_j_type_mem;
+        rs_wb           <=  rs_ex;
+        rt_wb           <=  rt_ex;
+        rd_wb           <=  rd_ex;
+    end
+
+    //always @ (negedge clk_tb)
+    //if (~reset_tb)
+    //begin
+    //    run (1);
+    //    if (T1.is_r_type_top) 
+    //    begin
+    //        if (!compare_r (pc, instr, rd, rs, rt, rd_val_dest, rs_val, rt_val))
+    //            $finish;
+    //    end
+    //    else if (T1.is_i_type_top)
+    //    begin
+    //        if (!compare_i (pc, instr, rs, rt, rs_val, rt_val_dest))
+    //            $finish;
+    //    end
+    //end
+
+    //always @ (negedge clk_tb)
+    //begin
+    //    if ((T1.instr_top == 'hc) && (T1.R1.reg_file[2] == 'ha))
+    //    begin
+    //        $display("End of simulation reached\n");
+    //        $finish;
+    //    end
+    //end
 
 endmodule
