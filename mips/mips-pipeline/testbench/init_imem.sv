c1a3e57b1c580474d976bba3534e1ab80e60c702
raulbehl <raulbehl@gmail.com>
2017-02-28 08:03:57 +0530

    [TB] - Added test_name valueplusarg

--- mips/mips-pipeline/testbench/init_imem.sv
+++ mips/mips-pipeline/testbench/init_imem.sv
@@ -1,12 +1,19 @@
 // Memory initialisation
 
-function void init_imem ();
+function void init_imem (string test_name);
     logic [31:0] instr_hex [1023:0];
     logic [31:0] pc_val [2048:0];
     
-    $readmemh ("instr_hex", instr_hex, 0, 999);
-    $readmemh ("pc_values_hex", pc_val, 0, 999);
+    string instr_hex_s;
+    string pc_values_hex_s;
     
+    instr_hex_s     = {test_name, ".hex"};
+    pc_values_hex_s = {test_name, "_pc.hex"};
+    
+
+    $readmemh (instr_hex_s, instr_hex, 0, 999);
+    $readmemh (pc_values_hex_s, pc_val, 0, 999);
+
     for (int i = 0; instr_hex[i]; i++) 
     begin
         //$display ("Loading %x at %x\n", instr_hex[i], pc_val[i]);
--- mips/mips-pipeline/testbench/top_tb.sv
+++ mips/mips-pipeline/testbench/top_tb.sv
@@ -4,12 +4,13 @@ module top_tb ();
 `include "testbench/init_imem.sv"
 `include "testbench/init_dmem.sv"
 `include "testbench/boot_code.sv"
-import "DPI-C" function void init ();
+import "DPI-C" function void init (string test_name);
 import "DPI-C" function void run (int cycles);
 import "DPI-C" function int compare_r (int pc, int instr, int rd, int rs, int rt, int rd_val, int rs_val, int rt_val);
 import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs_val, int rt_val);
 
-    logic clk_tb, reset_tb;
+    logic   clk_tb, reset_tb;
+    string  test_name;
 
     // Fetch stage signals
     logic[31:0]   pc_fetch;
@@ -61,30 +62,34 @@ import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs
     logic[31:0]   rt_val_wb;
     logic[31:0]   rd_val_dest_wb;
     logic[31:0]   rt_val_dest_wb;
+    // Retired
+    logic         instr_retired_top;
     
 
     // FETCH
-    assign pc_fetch         = T1.curr_pc_pc_reg_fetch;
-    assign instr_fetch      = T1.instr_pc_reg_fetch;
+    assign pc_fetch          = T1.curr_pc_pc_reg_fetch;
+    assign instr_fetch       = T1.instr_pc_reg_fetch;
 
     // ISSUE
-    assign is_r_type_iss    = T1.is_r_type_iss_ex;
-    assign is_i_type_iss    = T1.is_i_type_iss_ex;
-    assign is_j_type_iss    = T1.is_j_type_iss_ex;
-    assign rs_iss           = T1.rs_iss_ex;
-    assign rt_iss           = T1.rt_iss_ex;
-    assign rd_iss           = T1.rd_iss_ex;
+    assign is_r_type_iss     = T1.is_r_type_iss_ex;
+    assign is_i_type_iss     = T1.is_i_type_iss_ex;
+    assign is_j_type_iss     = T1.is_j_type_iss_ex;
+    assign rs_iss            = T1.rs_iss_ex;
+    assign rt_iss            = T1.rt_iss_ex;
+    assign rd_iss            = T1.rd_iss_ex;
 
     // EXECUTE
-    assign rd_val_ex        = T1.R1.reg_file[rd_ex];
-    assign rs_val_ex        = T1.R1.reg_file[rs_ex];
-    assign rt_val_ex        = T1.R1.reg_file[rt_ex];
+    assign rd_val_ex         = T1.R1.reg_file[rd_ex];
+    assign rs_val_ex         = T1.R1.reg_file[rs_ex];
+    assign rt_val_ex         = T1.R1.reg_file[rt_ex];
 
     // MEM
 
     // WRITE-BACK
-    //assign rt_val_dest  = T1.reg_wr_top ? T1.wr_data_rf_top : rt_val;
-    //assign rd_val_dest  = T1.reg_wr_top ? T1.wr_data_rf_top : rd_val;
+    assign rt_val_dest_wb    = T1.reg_wr_wb_ret ? T1.wr_data_rf_wb_ret : rt_val_wb;
+    assign rd_val_dest_wb    = T1.reg_wr_wb_ret ? T1.wr_data_rf_wb_ret : rd_val_wb;
+
+    // RETIRED
 
     top T1 (
         .clk (clk_tb),
@@ -95,13 +100,15 @@ import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs
     
     initial
     begin
-        init_imem ();
+        if (!($value$plusargs("test=%s", test_name)))
+          $fatal ("No test name given");
+        init_imem (test_name);
         init_dmem ();
         boot_code ();
-        init ();
+        init (test_name);
         $display ("CPU initialised\n");
         reset_tb = 1'b1;
-        # (T);
+        repeat (5) @(posedge clk_tb);
         reset_tb = 1'b0;
     end
 
@@ -123,6 +130,7 @@ import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs
     // EXECUTE
     always @ (posedge clk_tb)
     begin
+        //$display ("RD is %x\tRS is %x\tRT is %x\n", rd_iss, rs_iss, rt_iss);
         pc_ex           <=  pc_iss;
         instr_ex        <=  instr_iss;
         is_r_type_ex    <=  is_r_type_iss;
@@ -144,6 +152,9 @@ import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs
         rs_mem          <=  rs_ex;
         rt_mem          <=  rt_ex;
         rd_mem          <=  rd_ex;
+        rs_val_mem      <=  rs_val_ex;
+        rt_val_mem      <=  rt_val_ex;
+        rd_val_mem      <=  rd_val_ex;
     end
 
     // WRITE-BACK
@@ -151,37 +162,42 @@ import "DPI-C" function int compare_i (int pc, int instr, int rs, int rt, int rs
     begin
         pc_wb           <=  pc_mem;
         instr_wb        <=  instr_mem;
-        is_r_type_ex    <=  is_r_type_mem;
-        is_i_type_ex    <=  is_i_type_mem;
-        is_j_type_ex    <=  is_j_type_mem;
+        is_r_type_wb    <=  is_r_type_mem;
+        is_i_type_wb    <=  is_i_type_mem;
+        is_j_type_wb    <=  is_j_type_mem;
         rs_wb           <=  rs_ex;
         rt_wb           <=  rt_ex;
         rd_wb           <=  rd_ex;
+        rs_val_wb       <=  rs_val_mem;
+        rt_val_wb       <=  rt_val_mem;
+        rd_val_wb       <=  rd_val_mem;
     end
 
-    //always @ (negedge clk_tb)
-    //if (~reset_tb)
-    //begin
-    //    run (1);
-    //    if (T1.is_r_type_top) 
-    //    begin
-    //        if (!compare_r (pc, instr, rd, rs, rt, rd_val_dest, rs_val, rt_val))
-    //            $finish;
-    //    end
-    //    else if (T1.is_i_type_top)
-    //    begin
-    //        if (!compare_i (pc, instr, rs, rt, rs_val, rt_val_dest))
-    //            $finish;
-    //    end
-    //end
 
-    //always @ (negedge clk_tb)
-    //begin
-    //    if ((T1.instr_top == 'hc) && (T1.R1.reg_file[2] == 'ha))
-    //    begin
-    //        $display("End of simulation reached\n");
-    //        $finish;
-    //    end
-    //end
+    always @ (posedge instr_retired_top)
+    begin
+        run (1);
+        if (is_r_type_wb) 
+        begin
+            if (!compare_r (pc_wb, instr_wb, rd_wb, rs_wb, rt_wb, rd_val_dest_wb, rs_val_wb, rt_val_wb))
+                $finish;
+        end
+        else if (is_i_type_wb)
+        begin
+            if (!compare_i (pc_wb, instr_wb, rs_wb, rt_wb, rs_val_wb, rt_val_dest_wb))
+                $finish;
+        end
+        else
+            $fatal ("Incorrect instruction opcode");
+    end
+
+    always @ (posedge instr_retired_top)
+    begin
+        if ((instr_wb == 'hc) && (T1.R1.reg_file[2] == 'ha))
+        begin
+            $display("End of simulation reached\n");
+            $finish;
+        end
+    end
 
 endmodule
