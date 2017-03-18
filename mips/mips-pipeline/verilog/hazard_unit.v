b1fdcb10ae39f5fb82c850f34f97a43821607ffc
raulbehl <raulbehl@gmail.com>
2017-03-18 16:36:42 +0530

    [RTL] - Added flush_mem_hz signal
        - Since the branch would be resolved in the ex stage we would need to flush
          the mem register as well

--- mips/mips-pipeline/verilog/hazard_unit.v
+++ mips/mips-pipeline/verilog/hazard_unit.v
@@ -9,9 +9,12 @@ module hazard_unit
         input   wire        mem_to_reg_ex_mem_hz_i,
         input   wire        reg_wr_mem_wb_hz_i,
         input   wire        reg_wr_wb_ret_hz_i,
+        input   wire        branch_taken_ex_mem_hz_i,
+        input   wire        jump_iss_ex_hz_i,
         output  wire        stall_fetch_hz_o,
         output  wire        stall_iss_hz_o,
         output  wire        flush_ex_hz_o,
+        output  wire        flush_mem_hz_o,
         output  wire[1:0]   fwd_p1_ex_mem_hz_o,
         output  wire[1:0]   fwd_p2_ex_mem_hz_o
     );
@@ -41,12 +44,15 @@ module hazard_unit
                                : (reg_wr_wb_ret_hz_i & |rd_mem_wb_hz_i &
                                  (rd_wb_ret_hz_i == rt_ex_mem_hz_i)) ? 2'b01 
                                  : 2'b00;
-    // Branches would be resolved in the ISSUE (DECODE) stage
-    // the following signals should be based on those values
     assign stall_fetch_hz = 1'b0;
     assign stall_iss_hz = 1'b0;
+    // Jumps would be resolved in the ISSUE (DECODE) stage
+    // the following signals should be based on those values
     // This is required to flush the value in the EX Pipe register
     // to a NO-OP. 
-    assign flush_ex_hz = 1'b0;
+    assign flush_ex_hz = branch_taken_ex_mem_hz_i | jump_iss_ex_hz_i;
+    // Branches would be resolved in the EXECUTE stage
+    // the following signals should be based on those values
+    assign flush_mem_hz = branch_taken_ex_mem_hz_i;
 
 endmodule
