4d73a869a83669fa3c8aadb8b52ee91fdc056c74
raulbehl <raulbehl@gmail.com>
2017-03-18 16:37:31 +0530

    [RTL] - Added new signals to ex_pipe_reg
        - These signals help in resolving branch in the EX stage

--- mips/mips-pipeline/verilog/ex_pipe_reg.v
+++ mips/mips-pipeline/verilog/ex_pipe_reg.v
@@ -6,6 +6,8 @@ module ex_pipe_reg
         input   wire        reset,
         input   wire        clr,
         input   wire        valid_ex_pipe_reg_i,
+        input   wire[5:0]   op_ex_pipe_reg_i,
+        input   wire        branch_ex_pipe_reg_i,
         input   wire        reg_wr_ex_pipe_reg_i,
         input   wire        mem_to_reg_ex_pipe_reg_i,
         input   wire        mem_wr_ex_pipe_reg_i,
@@ -17,9 +19,12 @@ module ex_pipe_reg
         input   wire[4:0]   rd_ex_pipe_reg_i,
         input   wire[31:0]  r_data_p1_ex_pipe_reg_i,
         input   wire[31:0]  r_data_p2_ex_pipe_reg_i,
+        input   wire[31:0]  brn_eq_pc_ex_pipe_reg_i,
         input   wire[31:0]  sign_imm_ex_pipe_reg_i,
         input   wire[5:0]   shamt_ex_pipe_reg_i,
         output  wire        valid_ex_pipe_reg_o,
+        input   wire[5:0]   op_ex_pipe_reg_o,
+        input   wire        branch_ex_pipe_reg_o,
         output  wire        reg_wr_ex_pipe_reg_o,
         output  wire        mem_to_reg_ex_pipe_reg_o,
         output  wire        mem_wr_ex_pipe_reg_o,
@@ -31,11 +36,14 @@ module ex_pipe_reg
         output  wire[4:0]   rd_ex_pipe_reg_o,
         output  wire[31:0]  r_data_p1_ex_pipe_reg_o,
         output  wire[31:0]  r_data_p2_ex_pipe_reg_o,
+        input   wire[31:0]  brn_eq_pc_ex_pipe_reg_o,
         output  wire[31:0]  sign_imm_ex_pipe_reg_o,
         output  wire[5:0]   shamt_ex_pipe_reg_o
     );
 
     reg        valid_ex_pipe_reg;
+    reg[5:0]   op_ex_pipe_reg;
+    reg        branch_ex_pipe_reg;
     reg        reg_wr_ex_pipe_reg;
     reg        mem_to_reg_ex_pipe_reg;
     reg        mem_wr_ex_pipe_reg;
@@ -47,10 +55,13 @@ module ex_pipe_reg
     reg[4:0]   rd_ex_pipe_reg;
     reg[31:0]  r_data_p1_ex_pipe_reg;
     reg[31:0]  r_data_p2_ex_pipe_reg;
+    reg[31:0]  brn_eq_pc_ex_pipe_reg;
     reg[31:0]  sign_imm_ex_pipe_reg;
     reg[5:0]   shamt_ex_pipe_reg;
 
     assign valid_ex_pipe_reg_o          =  valid_ex_pipe_reg;
+    assign op_ex_pipe_reg_o             =  op_ex_pipe_reg;
+    assign branch_ex_pipe_reg_o         =  branch_ex_pipe_reg;
     assign reg_wr_ex_pipe_reg_o         =  reg_wr_ex_pipe_reg;
     assign mem_to_reg_ex_pipe_reg_o     =  mem_to_reg_ex_pipe_reg;
     assign mem_wr_ex_pipe_reg_o         =  mem_wr_ex_pipe_reg;
@@ -62,13 +73,16 @@ module ex_pipe_reg
     assign rd_ex_pipe_reg_o             =  rd_ex_pipe_reg;
     assign r_data_p1_ex_pipe_reg_o      =  r_data_p1_ex_pipe_reg;
     assign r_data_p2_ex_pipe_reg_o      =  r_data_p2_ex_pipe_reg;
+    assign brn_eq_pc_ex_pipe_reg_o      =  brn_eq_pc_ex_pipe_reg;
     assign sign_imm_ex_pipe_reg_o       =  sign_imm_ex_pipe_reg;
     assign shamt_ex_pipe_reg_o          =  shamt_ex_pipe_reg;
 
     always @(posedge clk or posedge reset)
-    if (reset || clr)
+    if (reset | clr)
     begin
         valid_ex_pipe_reg          <=  0;
+        op_ex_pipe_reg             <=  0;
+        branch_ex_pipe_reg         <=  0;
         reg_wr_ex_pipe_reg         <=  0;
         mem_to_reg_ex_pipe_reg     <=  0;
         mem_wr_ex_pipe_reg         <=  0;
@@ -80,12 +94,15 @@ module ex_pipe_reg
         rd_ex_pipe_reg             <=  0;
         r_data_p1_ex_pipe_reg      <=  0;
         r_data_p2_ex_pipe_reg      <=  0;
+        brn_eq_pc_ex_pipe_reg      <=  0;
         sign_imm_ex_pipe_reg       <=  0;
         shamt_ex_pipe_reg          <=  0;
     end
     else
     begin
         valid_ex_pipe_reg          <=  valid_ex_pipe_reg_i;
+        op_ex_pipe_reg             <=  op_ex_pipe_reg_i;
+        branch_ex_pipe_reg         <=  branch_ex_pipe_reg_i;
         reg_wr_ex_pipe_reg         <=  reg_wr_ex_pipe_reg_i;
         mem_to_reg_ex_pipe_reg     <=  mem_to_reg_ex_pipe_reg_i;
         mem_wr_ex_pipe_reg         <=  mem_wr_ex_pipe_reg_i;
@@ -97,6 +114,7 @@ module ex_pipe_reg
         rd_ex_pipe_reg             <=  rd_ex_pipe_reg_i;
         r_data_p1_ex_pipe_reg      <=  r_data_p1_ex_pipe_reg_i;
         r_data_p2_ex_pipe_reg      <=  r_data_p2_ex_pipe_reg_i;
+        brn_eq_pc_ex_pipe_reg      <=  brn_eq_pc_ex_pipe_reg_i;
         sign_imm_ex_pipe_reg       <=  sign_imm_ex_pipe_reg_i;
         shamt_ex_pipe_reg          <=  shamt_ex_pipe_reg_i;
     end
