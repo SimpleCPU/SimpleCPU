4e6b7e2a232fee81b04338b005c3ce13df56302f
raulbehl <raulbehl@gmail.com>
2017-03-19 12:53:39 +0530

    Added r_data_p2 signal to mem register for mem writes

--- mips/mips-pipeline/verilog/mem_pipe_reg.v
+++ mips/mips-pipeline/verilog/mem_pipe_reg.v
@@ -11,12 +11,14 @@ module mem_pipe_reg
         input   wire        mem_wr_mem_pipe_reg_i,
         input   wire[4:0]   rd_mem_pipe_reg_i,
         input   wire[31:0]  res_alu_mem_pipe_reg_i,
+        input   wire[31:0]  r_data_p2_mem_pipe_reg_i,
         output  wire        valid_mem_pipe_reg_o,
         output  wire        reg_wr_mem_pipe_reg_o,
         output  wire        mem_to_reg_mem_pipe_reg_o,
         output  wire        mem_wr_mem_pipe_reg_o,
         output  wire[4:0]   rd_mem_pipe_reg_o,
-        output  wire[31:0]  res_alu_mem_pipe_reg_o
+        output  wire[31:0]  res_alu_mem_pipe_reg_o,
+        input   wire[31:0]  r_data_p2_mem_pipe_reg_o
     );
 
     reg        valid_mem_pipe_reg;
@@ -25,6 +27,7 @@ module mem_pipe_reg
     reg        mem_wr_mem_pipe_reg;
     reg[4:0]   rd_mem_pipe_reg;
     reg[31:0]  res_alu_mem_pipe_reg;
+    reg[31:0]  r_data_p2_mem_pipe_reg;
 
     assign valid_mem_pipe_reg_o          =  valid_mem_pipe_reg;
     assign reg_wr_mem_pipe_reg_o         =  reg_wr_mem_pipe_reg;
@@ -32,6 +35,7 @@ module mem_pipe_reg
     assign mem_wr_mem_pipe_reg_o         =  mem_wr_mem_pipe_reg;
     assign rd_mem_pipe_reg_o             =  rd_mem_pipe_reg;
     assign res_alu_mem_pipe_reg_o        =  res_alu_mem_pipe_reg;
+    assign r_data_p2_mem_pipe_reg_o      =  r_data_p2_mem_pipe_reg;
 
     always @(posedge clk or posedge reset)
     if (reset | clr)
@@ -42,6 +46,7 @@ module mem_pipe_reg
         mem_wr_mem_pipe_reg         <=  0;
         rd_mem_pipe_reg             <=  0;
         res_alu_mem_pipe_reg        <=  0;
+        r_data_p2_mem_pipe_reg      <=  0;
     end
     else
     begin
@@ -51,6 +56,7 @@ module mem_pipe_reg
         mem_wr_mem_pipe_reg         <=  mem_wr_mem_pipe_reg_i;
         rd_mem_pipe_reg             <=  rd_mem_pipe_reg_i;
         res_alu_mem_pipe_reg        <=  res_alu_mem_pipe_reg_i;
+        r_data_p2_mem_pipe_reg      <=  r_data_p2_mem_pipe_reg_i;
     end
 
 endmodule
