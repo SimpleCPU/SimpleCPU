7f1ea9c58d67c1ff343cf3e61aa80f4154711417
raulbehl <raulbehl@gmail.com>
2017-03-24 00:49:54 +0530

    Added API header file

---
+++ mips/hex_gen/src/hex_gen_api.h
@@ -0,0 +1,17 @@
+/***************************************************************/
+/*                                                             */
+/* hex_gen_api.h                                               */
+/* Desclares a set of functions which can be                   */
+/* used to create an mips assembly test                        */
+/*                                                             */
+/***************************************************************/
+
+#include "hex_gen.h"
+
+/* Declare all the functions here */
+int get_rand_ls_addr ();
+int get_ls_addr ();
+int get_rand_pc ();
+void gen_r_instr (char*);
+void gen_i_instr (char*, int);
+void gen_j_instr (char*, int);
