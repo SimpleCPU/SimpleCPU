08ad7f85725bd112292364ea11a9beca9258f6c4
Rahul Behl <raulbehl@users.noreply.github.com>
2017-03-23 18:47:04 +0530

    Update two_bit_sat_counter.v
    - Added TODO for counters implementation
    - Updated comments

--- mips/mips-pipeline/verilog/two_bit_sat_counter.v
+++ mips/mips-pipeline/verilog/two_bit_sat_counter.v
@@ -16,11 +16,16 @@ module two_bit_sat_counter
     // but the branch was taken. Hence the prediction bits are updated
     //    op=1
     // 00 -----> 00
-    // similarly for other states as well
+    // The above means the prediction made was strongly not taken
+    // and the branch was also not taken. Hence there is no need
+    // to update the prediction bits
+    // Similar logic applies for other states as well
 
     always @ (*)
     begin
         case (count_i)
+            // TODO: Use an adder here and just update the op port
+            // of the adder for each case
             2'b00:  count = (op) ? count_i          : count_i + 2'b1;
             2'b01:  count = (op) ? count_i - 2'b1   : count_i + 2'b1;
             2'b10:  count = (op) ? count_i + 2'b1   : count_i - 2'b1;
