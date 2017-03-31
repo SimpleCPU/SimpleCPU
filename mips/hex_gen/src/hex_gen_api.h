2bc6dd3662d48fcc64c83a9b87aeb645b01ad523
raulbehl <raulbehl@gmail.com>
2017-03-31 12:10:14 +0530

    Refined few comments

--- mips/hex_gen/src/hex_gen_api.h
+++ mips/hex_gen/src/hex_gen_api.h
@@ -13,7 +13,11 @@
 int get_rand_ls_addr ();
 // Function to get an already used Load/Store addr
 int get_ls_addr ();
+// Function to get a random PC address
 int get_rand_pc ();
+// Function to generate a specific R-type instruction
 void gen_r_instr (char*);
+// Function to generate a specific I-type instruction
 void gen_i_instr (char*, int);
+// Function to generate a specific J-type instruction
 void gen_j_instr (char*, int);
