45272fc375a6019a18cb739b6cbd312e438f99cb
raulbehl <raulbehl@gmail.com>
2017-04-18 10:10:14 +0530

    Fixed compilation issues

--- mips/hex_gen/src/hex_gen_api.c
+++ mips/hex_gen/src/hex_gen_api.c
@@ -6,7 +6,6 @@
 /*                                                             */
 /***************************************************************/
 
-#include "hex_gen.h"
 #include "hex_gen_api.h"
 
 void gen_dir_j (char* op_str, int addr) {
@@ -15,26 +14,4 @@ void gen_dir_j (char* op_str, int addr) {
 //    target = (addr & 0xFFFFFFC)>>2;
 //    opcode = (J << 26) + target;
 //    return opcode;
-}
-
-void gen_dir_r_instr (char* instr_mnemonic) {
-    //int     opcode, shamt, funct;
-    //int     rs, rt, rd;
-    //int     hex_instr;
-    //int     funct_idx;
-}
-
-char* get_opcode (char* instr_mnemonic) {
-    // Take max size (len of SYSCALL opcode)
-    char* opcode = (char*) malloc (sizeof(char) * 7);
-    int len = strlen(instr_mnemonic);
-    int i;
-    // Look for first space to find the opcode
-    for (i = 0; i < len; i++) {
-        if (instr_mnemonic[i] != ' ') {
-            opcode[0] = instr_mnemonic[i];
-        }
-        else break;
-    }
-    return opcode;
 }
