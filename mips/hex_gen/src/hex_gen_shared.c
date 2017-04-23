3270de0b788af8c92419a18d3efea4da59d41857
raulbehl <raulbehl@gmail.com>
2017-04-23 21:45:38 +0530

    Added definitions of some global functions

---
+++ mips/hex_gen/src/hex_gen_shared.c
@@ -0,0 +1,23 @@
+/***************************************************************/
+/*                                                             */
+/* hex_gen_shared.c                                            */
+/* Defines globally used functions                             */
+/*                                                             */
+/*                                                             */
+/***************************************************************/
+
+#include "hex_gen_shared.h"
+
+/* Function to initialise Hex-Gen Tool  */
+/* All the global variables should be   */
+/* set to their initial value here      */
+void init_hex_gen () {
+    int i;
+    // PC and instr array should be initialised to 0
+    for (i = 0; i < IMEM_SIZE; i++) {
+        PC[i] = 0x0;
+        instr[i] = 0x0;
+    }
+    // Set instr_gen to 0
+    instr_gen = 0;
+}
