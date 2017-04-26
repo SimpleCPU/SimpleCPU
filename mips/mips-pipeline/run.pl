a055a65b17e0a4ca4f2ba01f13463b6955f92adc
Rahul Behl <raulbehl@users.noreply.github.com>
2017-04-26 16:40:39 +0530

    Update run.pl
    vlog should be invoked only for .v/.sv files

--- mips/mips-pipeline/run.pl
+++ mips/mips-pipeline/run.pl
@@ -71,8 +71,8 @@ else {
         system ("rm -rf vsim.wlf wlf* transcript work/");
         print ("vlib work\n");
         system ("vlib work");
-        print ("vlog testbench/* verilog/*\n");
-        system ("vlog testbench/* verilog/*");
+        print ("vlog testbench/*.sv testbench/*.v verilog/*.v\n");
+        system ("vlog testbench/*.sv testbench/*.v verilog/*.v");
     }
     # Run the simulation
     print ("vsim -c top_tb -sv_lib lib/iss -do \"run -all; exit\" +test=$test_name  | tee sim.log");
