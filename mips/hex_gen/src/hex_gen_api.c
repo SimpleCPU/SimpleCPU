4a36dac3e458c69760c3f93526216ff93dc89103
Rahul Behl <raulbehl@users.noreply.github.com>
2017-04-27 14:53:03 +0530

    Update hex_gen_api.c

--- mips/hex_gen/src/hex_gen_api.c
+++ mips/hex_gen/src/hex_gen_api.c
@@ -7,3 +7,16 @@
 /***************************************************************/
 
 #include "hex_gen_api.h"
+
+int find_reg (int reg_val) {
+  int i;
+  int found = 0;
+  
+  for (i = 0; i < MIPS_REG; i++) {
+    if (CURRENT_STATE.REGS[i] == reg_val) {
+      found = 1;
+      return found;
+    }
+  }
+  return found;
+}
