4eec1ad18ba4300ee9f7180aacaa82037fc3cbf4
raulbehl <raulbehl@gmail.com>
2017-04-30 11:10:22 +0530

    Updated SLLV function for shamt

--- mips/hex_gen/src/hex_gen_shared.h
+++ mips/hex_gen/src/hex_gen_shared.h
@@ -68,7 +68,7 @@ void gen_r_type_MULTU   (int, int, int, int, ...);
 void gen_r_type_NOR     (int, int, int, int, ...);
 void gen_r_type_OR      (int, int, int, int, ...);
 void gen_r_type_SLL     (int, int, int, int, int, ...);
-void gen_r_type_SLLV    (int, int, int, int, ...);
+void gen_r_type_SLLV    (int, int, int, int, int, ...);
 void gen_r_type_SLT     (int, int, int, int, ...);
 void gen_r_type_SLTU    (int, int, int, int, ...);
 void gen_r_type_SRA     (int, int, int, int, ...);
