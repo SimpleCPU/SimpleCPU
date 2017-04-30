2581f095fef69acf86d29fbbb93294100d9dba39
raulbehl <raulbehl@gmail.com>
2017-05-01 01:10:22 +0530

    Updated few api functions

--- mips/hex_gen/src/hex_gen_instr_api.c
+++ mips/hex_gen/src/hex_gen_instr_api.c
@@ -24,321 +24,607 @@ void gen_r_type_ADD (int RD, int RS, int RT, int set_rval, ...) {
         rs_val  = va_arg (valist, int);
         rt_val  = va_arg (valist, int);
         va_end (valist);
-        // RS = find_reg (rs_val) ? get_reg (rs_val) : gen_i_type_ORI (RS, R0, rs_val);
-        // RT = find_reg (rt_val) ? get_reg (rt_val) : gen_i_type_ORI (RT, R0, rt_val);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
     }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_ADDU (int RD, int RS, int RT) {
+void gen_r_type_ADDU (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = ADDU;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_AND (int RD, int RS, int RT) {
+void gen_r_type_AND (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = AND;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_DIV (int RD, int RS, int RT) {
+void gen_r_type_DIV (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = DIV;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_DIVU (int RD, int RS, int RT) {
+void gen_r_type_DIVU (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = DIVU;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_JALR (int RD, int RS, int RT) {
+void gen_r_type_JALR (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = JALR;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_JR (int RD, int RS, int RT) {
+void gen_r_type_JR (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = JR;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_MFHI (int RD, int RS, int RT) {
+void gen_r_type_MFHI (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = MFHI;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_MFLO (int RD, int RS, int RT) {
+void gen_r_type_MFLO (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = MFLO;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_MTHI (int RD, int RS, int RT) {
+void gen_r_type_MTHI (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = MTHI;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_MTLO (int RD, int RS, int RT) {
+void gen_r_type_MTLO (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = MTLO;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_MULT (int RD, int RS, int RT) {
+void gen_r_type_MULT (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = MULT;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_MULTU (int RD, int RS, int RT) {
+void gen_r_type_MULTU (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = MULTU;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_NOR (int RD, int RS, int RT) {
+void gen_r_type_NOR (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = NOR;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_OR (int RD, int RS, int RT) {
+void gen_r_type_OR (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = OR;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SLL (int RD, int RS, int RT) {
+void gen_r_type_SLL (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SLL;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SLLV (int RD, int RS, int RT) {
+void gen_r_type_SLLV (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SLLV;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SLT (int RD, int RS, int RT) {
+void gen_r_type_SLT (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SLT;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SLTU (int RD, int RS, int RT) {
+void gen_r_type_SLTU (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SLTU;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SRA (int RD, int RS, int RT) {
+void gen_r_type_SRA (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SRA;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SRAV (int RD, int RS, int RT) {
+void gen_r_type_SRAV (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SRAV;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SRL (int RD, int RS, int RT) {
+void gen_r_type_SRL (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SRL;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SRLV (int RD, int RS, int RT) {
+void gen_r_type_SRLV (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SRLV;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SUB (int RD, int RS, int RT) {
+void gen_r_type_SUB (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SUB;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SUBU (int RD, int RS, int RT) {
+void gen_r_type_SUBU (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SUBU;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_SYSCALL (int RD, int RS, int RT) {
+void gen_r_type_SYSCALL (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = SYSCALL;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
 
-void gen_r_type_XOR (int RD, int RS, int RT) {
+void gen_r_type_XOR (int RD, int RS, int RT, int set_rval, ...) {
     int     funct;
     int     shamt;
     int     vopt;
+    int     rs_val;
+    int     rt_val;
+    va_list valist;
 
     funct   = XOR;
     shamt   = 0;
     vopt    = 5;
+    if (set_rval) {
+        va_start (valist, set_rval);
+        rs_val  = va_arg (valist, int);
+        rt_val  = va_arg (valist, int);
+        va_end (valist);
+        //gen_i_type_ORI (RS, R0, rs_val);
+        //gen_i_type_ORI (RT, R0, rt_val);
+    }
     make_room ();
     gen_r_instr (vopt, funct, RD, RS, RT, shamt);
 }
