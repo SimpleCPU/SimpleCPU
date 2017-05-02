a10a8281de492b592ad26197509fea233d6fb239
raulbehl <raulbehl@gmail.com>
2017-05-03 00:08:51 +0530

    Updated test for I-type instr

--- mips/hex_gen/src/gen_user_test.c
+++ mips/hex_gen/src/gen_user_test.c
@@ -4,4 +4,5 @@ void gen_user_test () {
     printf ("Generating user test...\n\n");
     gen_r_type_ADD (R4, R2, R1, 0);
     gen_r_type_ADD (R24, R22, R12, 0);
+    gen_i_type_ADDI (R24, R22, 0xFFFFF, 0);
 }
