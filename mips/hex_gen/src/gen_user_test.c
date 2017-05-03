405a38b13ff9e2214702188833e37ca9ea9ca028
raulbehl <raulbehl@gmail.com>
2017-05-03 15:03:09 +0530

    Fixed the IMM to be 16-bit number

--- mips/hex_gen/src/gen_user_test.c
+++ mips/hex_gen/src/gen_user_test.c
@@ -2,7 +2,7 @@
 
 void gen_user_test () {
     printf ("Generating user test...\n\n");
-    gen_r_type_ADD (R4, R2, R1, 0);
-    gen_r_type_ADD (R24, R22, R12, 0);
-    gen_i_type_ADDI (R24, R22, 0xFFFFF, 0);
+    gen_r_type_ADD (R4, R2, R1, 1, 16, 16);
+    gen_r_type_ADD (R24, R22, R12, 1, 24, 20);
+    gen_i_type_ADDI (R24, R22, 0xFFFF, 1, 0);
 }
