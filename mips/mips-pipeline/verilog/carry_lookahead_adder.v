1e056e906abdd62faf56ae94dc884575262f6dc6
Rahul Behl <raulbehl@users.noreply.github.com>
2017-06-01 20:49:13 +0530

    Update carry_lookahead_adder.v
    Added v flag calculation logic

--- mips/mips-pipeline/verilog/carry_lookahead_adder.v
+++ mips/mips-pipeline/verilog/carry_lookahead_adder.v
@@ -6,7 +6,8 @@ module carry_lookahead_adder
         input   wire[3:0]   op2,
         input   wire        cin,
         output  wire[3:0]   s,
-        output  wire        cout
+        output  wire        cout,
+        output  wire        v
     );
 
     wire[3:0]   g;
@@ -14,11 +15,12 @@ module carry_lookahead_adder
     wire[3:0]   gen_cout;
 
     assign  cout = g[3] | (p[3] & gen_cout[3]);
+    assign  v    = gen_cout[3] ^ cout;
 
-    reduced_full_adder  RFA1(op1[0], op2[0], cin,         g[0], p[0], s[0]);
-    reduced_full_adder  RFA2(op1[1], op2[1], gen_cout[1], g[1], p[1], s[1]);
-    reduced_full_adder  RFA3(op1[2], op2[2], gen_cout[2], g[2], p[2], s[2]);
-    reduced_full_adder  RFA4(op1[3], op2[3], gen_cout[3], g[3], p[3], s[3]);
+    reduced_full_adder  RFA0(op1[0], op2[0], cin,         g[0], p[0], s[0]);
+    reduced_full_adder  RFA1(op1[1], op2[1], gen_cout[1], g[1], p[1], s[1]);
+    reduced_full_adder  RFA2(op1[2], op2[2], gen_cout[2], g[2], p[2], s[2]);
+    reduced_full_adder  RFA3(op1[3], op2[3], gen_cout[3], g[3], p[3], s[3]);
 
     carry_lookahead_gen CLG1(g, p, cin, gen_cout);
 
