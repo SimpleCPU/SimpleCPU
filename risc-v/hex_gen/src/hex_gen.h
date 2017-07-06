31509499abc021a927f003d70e20a9f5516defc4
raulbehl <raulbehl@gmail.com>
2017-07-06 22:11:59 +0530

    [RISCV] - Added correct opcode arrays in hex-gen

--- risc-v/hex_gen/src/hex_gen.h
+++ risc-v/hex_gen/src/hex_gen.h
@@ -35,102 +35,70 @@ extern int prev_pc;
 FILE* pc_hex_val;
 FILE* instr_hex_val;
 
-const int funct_val_r_type[13] = {
-    ADD,        ADDU,       AND,
-    /*DIV,        DIVU,       JALR,*/
-    /*JR,         MFHI,       MFLO,*/
-    /*MTHI,       MTLO,       MULT,*/
-    /*MULTU,*/      NOR,        OR,
-    SLL,        /*SLLV,*/       SLT,
-    SLTU,       SRA,        /*SRAV,*/
-    SRL,        /*SRLV,*/       SUB,
-    SUBU,       /*SYSCALL,*/    XOR
+const int funct_val_r_type[10] = {
+    SLL,     SRL,       SRA,
+    ADD,     SUB,       AND,
+    OR,      XOR,       SLT,
+    SLTU
 };
-const char* funct_str_r_type[13] = {
-    "ADD",        "ADDU",       "AND",
-    /*DIV,        DIVU,       JALR,*/
-    /*JR,         MFHI,       MFLO,*/
-    /*MTHI,       MTLO,       MULT,*/
-    /*MULTU,*/      "NOR",        "OR",
-    "SLL",        /*SLLV,*/       "SLT",
-    "SLTU",       "SRA",        /*SRAV,*/
-    "SRL",        /*SRLV,*/       "SUB",
-    "SUBU",       /*SYSCALL,*/    "XOR"
+const char* funct_str_r_type[10] = {
+    "SLL",     "SRL",       "SRA",
+    "ADD",     "SUB",       "AND",
+    "OR",      "XOR",       "SLT",
+    "SLTU"
 };
-/*const char* funct_str_r_type[27] = {
-    "ADD",        "ADDU",       "AND",
-    "DIV",        "DIVU",       "JALR",
-    "JR",         "MFHI",       "MFLO",
-    "MTHI",       "MTLO",       "MULT",
-    "MULTU",      "NOR",        "OR",
-    "SLL",        "SLLV",       "SLT",
-    "SLTU",       "SRA",        "SRAV",
-    "SRL",        "SRLV",       "SUB",
-    "SUBU",       "SYSCALL",    "XOR"
-};*/
-const int opcode_val_i_type[17] = {
-    ADDI,       ADDIU,      ANDI,
-    BEQ,        BGEZ,       BGEZAL,
-    BGTZ,       BLEZ,       BLTZ,
-    BLTZAL,     BNE,        /*LB,
-    LBU,        LH,         LHU,
-    LUI,*/        LW,         ORI,
-    /*SB,         SH,*/         SW,
-    SLTI,       SLTIU,      XORI
+const int opcode_val_i_type[12] = {
+    ADDI,       SLTI,       SLTIU,
+    ANDI,       ORI,        XORI,
+    JALR,       LB,         LH,
+    LW,         LBU,        LHU
 };
-const char* opcode_str_i_type[17] = {
-    "ADDI",       "ADDIU",      "ANDI",
-    "BEQ",        "BGEZ",       "BGEZAL",
-    "BGTZ",       "BLEZ",       "BLTZ",
-    "BLTZAL",     "BNE",        /*LB,
-    LBU,        LH,         LHU,
-    LUI,*/        "LW",         "ORI",
-    /*SB,         SH,*/         "SW",
-    "SLTI",       "SLTIU",      "XORI"
+const char* opcode_str_i_type[12] = {
+    "ADDI",       "SLTI",       "SLTIU",
+    "ANDI",       "ORI",        "XORI",
+    "JALR",       "LB",         "LH",
+    "LW",         "LBU",        "LHU"
 };
-/*const char* opcode_str_i_type[24] = {
-    "ADDI",       "ADDIU",      "ANDI",
-    "BEQ",        "BGEZ",       "BGEZAL",
-    "BGTZ",       "BLEZ",       "BLTZ",
-    "BLTZAL",     "BNE",        "LB",
-    "LBU",        "LH",         "LHU",
-    "LUI",        "LW",         "ORI",
-    "SB",         "SH",         "SW",
-    "SLTI",       "SLTIU",      "XORI"
-};*/
-const int opcode_val_j_type[2] = {
-    J,          JAL
+const int opcode_val_s_type[3] = {
+    SB,         SH,         SW
 };
-const char* opcode_str_j_type[2] = {
-    "J",          "JAL"
+const char* opcode_str_s_type[3] = {
+    "SB",         "SH",         "SW"
 };
-/*const char* register_str[] = {	
-    "$0",       "$at",      "$v0",  
-    "$v1",      "$a0",      "$a1",  
-    "$a2",      "$a3",      "$t0",  
-    "$t1",      "$t2",      "$t3",  
-    "$t4",      "$t5",      "$t6",  
-    "$t7",      "$s0",      "$s1",  
-    "$s2",      "$s3",      "$s4",  
-    "$s5",      "$s6",      "$s7", 
-    "$t8",      "$t9",      "$k0",  
-    "$k1",      "$gp",      "$sp", 
-    "$fp",      "$ra",             
-};*/
+const int opcode_val_b_type[6] = {
+    BEQ,        BNE,        BLT,
+    BLTU,       BGE,        BGEU
+};
+const char* opcode_str_b_type[6] = {
+    "BEQ",        "BNE",        "BLT",
+    "BLTU",       "BGE",        "BGEU"
+};
+const int opcode_val_u_type[2] = {
+    LUI,        AUIPC
+};
+const char* opcode_str_u_type[2] = {
+    "LUI",        "AUIPC"
+};
+const int opcode_val_j_type[1] = {
+    JAL
+};
+const char* opcode_str_j_type[1] = {
+    "JAL"
+};
 /* Currently the RTL only supports */
 /* registers in the following format */
 const char* register_str[] = {	
-    "R0",       "R1",       "R2",  
-    "R3",       "R4",       "R5",  
-    "R6",       "R7",       "R8",  
-    "R9",       "R10",      "R11",  
-    "R12",      "R13",      "R14",  
-    "R15",      "R16",      "R17",  
-    "R18",      "R19",      "R20",  
-    "R21",      "R22",      "R23", 
-    "R24",      "R25",      "R26",  
-    "R27",      "R28",      "R29", 
-    "R30",      "R31",             
+    "X0",       "X1",       "X2",  
+    "X3",       "X4",       "X5",  
+    "X6",       "X7",       "X8",  
+    "X9",       "X10",      "X11",  
+    "X12",      "X13",      "X14",  
+    "X15",      "X16",      "X17",  
+    "X18",      "X19",      "X20",  
+    "X21",      "X22",      "X23", 
+    "X24",      "X25",      "X26",  
+    "X27",      "X28",      "X29", 
+    "X30",      "X31",             
 };
 
 #endif
