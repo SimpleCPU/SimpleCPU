609d9f33d9ce968c836b50780238d67b7ac5c8a1
Rahul Behl <raulbehl@gmail.com>
2017-09-13 08:09:52 +0530

    Updated comments

--- risc-v/iss/src/sim.c
+++ risc-v/iss/src/sim.c
@@ -745,7 +745,7 @@ void decode_j (uint32_t instr_opcode) {
 }
 
 void process_instruction() {
-    /* execute one instruction here. You should use CURRENT_STATE and modify
+    /* Execute one instruction here. You should use CURRENT_STATE and modify
      * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
      * access memory. */
     instr_opcode = mem_read_32(CURRENT_STATE.PC);
