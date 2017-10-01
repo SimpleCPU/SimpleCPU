86386b3fd77e7192db24581ec210adb3e8742dfb
raulbehl <raulbehl@gmail.com>
2017-10-01 12:21:21 +0530

    Updated the module names

--- risc-v/riscv-pipeline/verilog/sign_extnd_20bit.v
+++ risc-v/riscv-pipeline/verilog/sign_extnd_20bit.v
@@ -1,9 +1,9 @@
-module u_type_sign_extnd
+module sign_extnd_20bit
     (
         input   wire[19:0]  instr_imm_i,
         output  wire[31:0]  sign_extnd_instr_imm_o
     );
 
-    assign  sign_extnd_instr_imm_o = {{12{instr_imm_i[19]}}, instr_imm_i};
+    assign  sign_extnd_instr_imm_o = {{11{instr_imm_i[19]}}, instr_imm_i, 1'b0};
 
 endmodule
