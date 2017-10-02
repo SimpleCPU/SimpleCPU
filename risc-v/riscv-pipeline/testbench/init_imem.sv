339c4a4fbaa3a24db15ef57121e718738e052a0d
raulbehl <raulbehl@gmail.com>
2017-10-02 17:43:44 +0530

    Fixed address calculation

--- risc-v/riscv-pipeline/testbench/init_imem.sv
+++ risc-v/riscv-pipeline/testbench/init_imem.sv
@@ -4,6 +4,9 @@ function void init_imem (string test_name);
     logic [31:0] instr_hex [2047:0];
     logic [31:0] pc_val [2047:0];
     
+    parameter instr_seg_begin = 32'h2000,
+              instr_seg_size  = 32'h1FFF;
+
     string instr_hex_s;
     string pc_values_hex_s;
     
@@ -17,7 +20,7 @@ function void init_imem (string test_name);
     for (int i = 0; instr_hex[i]; i++) 
     begin
         //$display ("Loading %x at %x\n", instr_hex[i], pc_val[i]);
-        T1.I_MEM1.imem [pc_val[i]>>2] = instr_hex[i];
+        T1.I_MEM1.imem [(pc_val[i]-instr_seg_begin)>>2] = instr_hex[i];
     end
 
 endfunction
