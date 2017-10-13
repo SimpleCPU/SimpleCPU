75d21f4eece951fbd3d7142b6f6d18fc945edd64
raulbehl <raulbehl@gmail.com>
2017-10-13 18:21:49 +0530

    [RISCV] - Fixed sign extend unit

--- risc-v/riscv-pipeline/verilog/sign_extnd_12bit.v
+++ risc-v/riscv-pipeline/verilog/sign_extnd_12bit.v
@@ -1,9 +1,26 @@
+`include "verilog/riscv_instr_defines.v"
+
 module sign_extnd_12bit
     (
         input   wire[11:0]  instr_imm_i,
+        input   wire[6:0]   instr_type_i,
         output  wire[31:0]  sign_extnd_instr_imm_o
     );
 
-    assign  sign_extnd_instr_imm_o = {{18{instr_imm_i[11]}}, instr_imm_i, 1'b0};
+    reg [31:0] sign_extnd_instr_imm;
+
+    assign  sign_extnd_instr_imm_o = sign_extnd_instr_imm;
+
+    always @ (instr_imm_i or instr_type_i)
+    begin
+        case (instr_type_i)
+            `I_TYPE_0,
+            `I_TYPE_1,
+            `I_TYPE_2,
+            `S_TYPE     : sign_extnd_instr_imm = {{20{instr_imm_i[11]}}, instr_imm_i};
+            `B_TYPE     : sign_extnd_instr_imm = {{19{instr_imm_i[11]}}, instr_imm_i, 1'b0};
+            default     : sign_extnd_instr_imm = {{20{instr_imm_i[11]}}, instr_imm_i};
+        endcase
+    end
 
 endmodule
