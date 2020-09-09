434e24e042fbe7686171e17a9924f1f894e847bd
Rahul Behl <rahulbehl@Sahils-MacBook-Pro.local>
2020-09-09 12:05:17 +0530

    Removing old files

--- uvm_tb/sha1_uvm_tb/tb_uvm/sha256_sequencer.sv
+++
@@ -1,8 +0,0 @@
-// SHA-256 Sequencer
-`include "uvm_macros.svh"
-`include "uvm_pkg.sv"
-import uvm_pkg::*;
-
-`include "sha256_transaction.sv"
-
-typedef uvm_sequencer # (sha256_transaction) sha256_sequencer;
